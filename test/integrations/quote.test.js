de754f97fb2df36ae7a06a1198b5e33ceb0b51c6
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2022-06-06 00:35:10 +0100

    chore: added test.

--- test/integrations/quote.test.js
+++ test/integrations/quote.test.js
@@ -5,7 +5,12 @@ const quidax = require("./quidax_object");
 describe("Quotes Collection", () => {
     it("list all markets", async () => {
         try {
-            const data = await quidax.quotes.quote("btcngn", "btc", "ask", 1)
+            const data = await quidax.quotes.quote({
+                "market": "btcngn",
+                "unit": "ngn",
+                "kind": "ask",
+                "total": "200000"
+            })
             markets = data.data
             expect(data).to.be.an('object')
             expect(data.status).to.equal("success")
